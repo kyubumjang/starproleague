6f31c98f188f74b5a08e5741951f2e4003f2ab14
Lawrene Jang <jkb2221@gmail.com>
2023-07-10 19:49:32 +0900

    🧹 chore: add i18next-resources-to-backend, next-i18next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const { i18n } = require("./next-i18next.config");
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
@@ -16,7 +17,17 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  i18n,
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      if (!config.resolve.fallback) {
+        config.resolve.fallback = {};
+      }
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 };
 
 module.exports = withVanillaExtract(withMDX(nextConfig));
